52663dcb6e7e61d4a35903fd4533dbcb66ca2bfe
Sergey Tvardovsky <87kell@gmail.com>
2024-12-15 13:34:12 +0200

    todo-6 add 'sleeper' helper @repo/helpers

--- packages/helpers/src/query/query.ts
+++ packages/helpers/src/query/query.ts
@@ -1,5 +1,12 @@
 import type { QueryAsyncItem } from './types'
 
+/**
+ * Executes an asynchronous request from a `QueryAsyncItem`, and handles the result or failure.
+ *
+ * @param item - A `QueryAsyncItem` object containing the asynchronous request (`req`) and an optional callback (`callback`) to be called once the request is complete.
+ *
+ * @throws {Error} - Will throw an error if the asynchronous request fails.
+ */
 const getAsyncFunction = async (item: QueryAsyncItem) => {
   try {
     await item.req()
@@ -13,6 +20,16 @@ const getAsyncFunction = async (item: QueryAsyncItem) => {
   }
 }
 
+/**
+ * Executes an array of asynchronous queries sequentially.
+ *
+ * Each query is executed in the order they appear in the `items` array.
+ * If any query fails, the execution will stop and throw an error.
+ *
+ * @param items - An array of `QueryAsyncItem` objects, each containing a request function (`req`) and an optional callback (`callback`).
+ *
+ * @throws {Error} - Will throw an error if any of the asynchronous requests fail.
+ */
 export const exeQuery = async (items: QueryAsyncItem[]) => {
   for (const item of items) {
     await getAsyncFunction(item)
---
+++ packages/helpers/src/sleeper/sleeper.ts
@@ -0,0 +1,27 @@
+/**
+ * Sleep until the condition in the callback becomes true or the maximum retry count is reached.
+ *
+ * @param maxRepeats - Maximum number of attempts to check the condition (retries).
+ * @param timeout - Timeout in milliseconds between each retry.
+ * @param cb - A callback 'reactive' function that should return a boolean value. The function will return `true` when the condition is met, triggering the wake-up.
+ *
+ * @returns 'wake' if the condition is met within the given retries, 'failed' if the maximum retries are reached.
+ */
+export const sleeper = async (
+  maxRepeats: number = 10,
+  timeout: number = 100,
+  cb: () => boolean
+): Promise<'wake' | 'failed'> => {
+  let retries = 0
+
+  while (retries <= maxRepeats) {
+    if (cb()) {
+      return 'wake'
+    }
+
+    retries++
+    await new Promise(resolve => setTimeout(resolve, timeout))
+  }
+
+  return 'failed'
+}
